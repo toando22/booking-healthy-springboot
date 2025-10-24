1b8addd6bfcc177df2ed69260e7fbe454e37624c
Do Duy Toan <doduytoan2201@gmail.com>
2025-10-24 13:48:56 +0700

    Khởi đầu

---
+++ src/main/java/com/bookinghealthy/booking_healthy/BookingHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.bookinghealthy.booking_healthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHealthyApplication.class, args);
+	}
+
+}
