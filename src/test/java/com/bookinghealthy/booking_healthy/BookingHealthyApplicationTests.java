4f8cf8513fbf2d7540529a00ae9542b1f415bf26
HoangAnh176 <vuhoanganh1706@gmail.com>
2025-11-05 15:59:14 +0700

    Resolve merge conflict and keep local application.properties

---
+++ src/test/java/com/bookinghealthy/booking_healthy/BookingHealthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinghealthy.booking_healthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHealthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
